6545bd13021f30fcc4d20ad3ebba9bb725bc26f3
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-01-29 05:19:51 +0000

    dir-locals: make examples by default

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil      . ((compile-command . "make MODE=debug -k")
+((nil      . ((compile-command . "make -k MODE=debug examples")
               (+license/license-choice . "MIT License")))
  (c-mode   . ((mode . clang-format))))
